29d38a4fe72b0b7e23417ace78f2da7616f7d5b5
nm1996 <milanovic.nikola11@gmail.com>
2024-06-05 16:56:29 +0200

    cors add

--- main.go
+++ main.go
@@ -7,22 +7,36 @@ import (
 	handler "simpleGoLang/handlers"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == "OPTIONS" {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	db := GetDBConnection()
-
 	defer db.Close()
 
-	http.HandleFunc("/api/login", func(w http.ResponseWriter, r *http.Request) {
-		handler.GetUserByNameAndPasswordHandler(w, r, db)
-	})
-
-	http.HandleFunc("/api/register", func(w http.ResponseWriter, r *http.Request) {
-		handler.InsertUserHandler(w, r, db)
-	})
-
-	http.HandleFunc("/api/course", func(w http.ResponseWriter, r *http.Request) {
-		handler.GetCoursesHandler(w, r, db)
-	})
+	http.Handle("/api", corsMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/login":
+			handler.GetUserByNameAndPasswordHandler(w, r, db)
+		case "/register":
+			handler.InsertUserHandler(w, r, db)
+		case "/course":
+			handler.GetCoursesHandler(w, r, db)
+		default:
+			http.NotFound(w, r)
+			return
+		}
+	})))
 
 	fmt.Println("Server started at :8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
